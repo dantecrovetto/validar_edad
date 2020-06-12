9ec2759dc8202ed2ff5eb67d3d5ce6c2db014487
Dante Crovetto <dante.crovetto@gmail.com>
2020-06-12 03:43:34 -0400

    Desafío validar edad

---
+++ validar_edad.rb
@@ -0,0 +1,13 @@
+edad = 0
+
+def validar_edad(edad)
+    if edad >= 18
+        puts "es mayor"
+    else
+        puts "es menor"
+    end
+end
+
+validar_edad rand()
+validar_edad rand()
+validar_edad rand()
